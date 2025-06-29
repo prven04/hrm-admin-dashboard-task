c4d142dc376d631210de1de008c64db179b3e147
prven04 <prven04@gmail.com>
2025-06-29 16:59:54 +0530

    build issue fixed

--- src/app/components/employee-performance/employee-performance.ts
+++ src/app/components/employee-performance/employee-performance.ts
@@ -2,18 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-employee-performance',
-  imports: [
-    CommonModule,
-    RouterLink,
-    MatIconModule,
-    MatButtonModule,
-    NgxChartsModule,
-  ],
+  imports: [CommonModule, MatIconModule, MatButtonModule, NgxChartsModule],
 
   templateUrl: './employee-performance.html',
   styleUrl: './employee-performance.css',
